7c2ccf91796d1dd663624e4e87259d88f1fd5b78
Well Liquor <liquorwell36@gmail.com>
2024-02-05 08:13:11 +0900

    find element

--- test/specs/home.js
+++ test/specs/home.js
@@ -9,11 +9,17 @@ describe('Home', () => {
         //Assert title
         await expect(browser).toHaveTitle('Practice E-Commerce Site – SDET Unicorns');
     });
-});
 
-describe('About', () => {
     it('Open URL & assert URL', async () => {
         await browser.url('/about/');
         await expect(browser).toHaveUrl('https://practice.sdetunicorns.com/about/');
+    });
+
+    it('Click get-started btn & assert url contains get-started text', async () => {
+        //Open URL
+        await browser.url('https://practice.sdetunicorns.com/');
+        await $('#get-started').click();
+        //Assert url contains get-started text
+        await expect(browser).toHaveUrlContaining('get-started');
     });
 });
